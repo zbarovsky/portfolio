270c499739bb4a51461563d15d9bddfeabf43001
zbarovsky <zachary.barovsky@gmail.com>
2020-08-07 16:16:11 -0700

    added test pages demo

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/navbar'
+
+export default function About () {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1>Hi! My name is Zack 🎸</h1>
+            </div>
+        </div>
+    )
+}
---
+++ pages/test.js
@@ -0,0 +1,30 @@
+export default function Test({queryParam}) {
+    let name = queryParam
+
+    return (
+        <>
+            <h1 id='hello'>
+                Oh, Hello {name}
+            </h1>
+            <style jsx>
+                {`
+                    #hello {
+                        color: hotpink;
+                    }
+                `}
+            </style>
+        </>
+    )
+}
+
+export function getServerSideProps ({query}) {
+    // get query param from url
+    let queryParam = query.name
+
+    return {
+        props: {
+            queryParam,
+        }
+    }
+
+}
