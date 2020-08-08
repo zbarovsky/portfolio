a703333dd4e18412593271f315ac6c94b055563f
zbarovsky <zachary.barovsky@gmail.com>
2020-08-08 10:35:05 -0700

    added Navbar to index page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Navbar />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
