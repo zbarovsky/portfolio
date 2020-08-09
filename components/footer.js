e4a795849c241565d13ef2d7d5a6cec8d98162ed
zbarovsky <zachary.barovsky@gmail.com>
2020-08-08 18:26:39 -0700

    imported svg imgs into techstack to add to index

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,6 @@ export default function Footer () {
         <div>
             <hr />
             <p className='copywrite'>© Zack Barovsky 2020</p>
-            <p>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
         </div>
     )
 }
