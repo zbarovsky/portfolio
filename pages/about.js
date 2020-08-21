36afeb8e4f0d884bcabb48a2fea23cb9456ace05
zbarovsky <zachary.barovsky@gmail.com>
2020-08-21 15:13:56 -0700

    updatede resume button and updated resume pdf itself

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ export default function About () {
             <div className='aboutMeTitle'>
                 <h3>About Me</h3>
                 <div className='aboutMeBio'>
-                    <a className ='resume' href='/resume.pdf' target='_blank'>RESUME</a>
+                    <button className='resume'><a href='/resume.pdf' target='_blank'>Resume</a></button>
                     <p>I am a software engineer based in the Seattle area. After years of social work, I decided to follow my passion for coding and collaborating with others by using code to bring action to their goals. When I'm not working, I'm often found either playing music or videogames.</p>
                     <a href='https://www.linkedin.com/in/zbarovsky/' target='_blank'><svg className='icon' viewBox="0 0 128 128">
                         <path fill='#0072b1' class="cls-1" d="M116,3H12a8.91,8.91,0,0,0-9,8.8V116.22A8.91,8.91,0,0,0,12,125H116a8.93,8.93,0,0,0,9-8.81V11.77A8.93,8.93,0,0,0,116,3Z"></path><path class="cls-2" d="M21.06,48.73H39.17V107H21.06Zm9.06-29a10.5,10.5,0,1,1-10.5,10.49,10.5,10.5,0,0,1,10.5-10.49"></path><path class="cls-2" d="M50.53,48.73H67.89v8h.24c2.42-4.58,8.32-9.41,17.13-9.41C103.6,47.28,107,59.35,107,75v32H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87,7.36-10.87,15V107H50.53Z"></path>
