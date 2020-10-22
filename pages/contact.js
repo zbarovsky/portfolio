e63fe043a10aec60eab315399415cbe612e42135
Zack Barovsky <zachary.barovsky@gmail.com>
2020-10-22 11:15:14 -0700

    added alt tags for contact icons

--- pages/contact.js
+++ pages/contact.js
@@ -12,12 +12,12 @@ export default function Contact () {
                 <p className='pl-3'>Connect with me on any of the platforms below, OR checkout my resume and shoot me an email if you're interested in collaborating!</p>
             </div>
             <div className='socialmediaSet pl-3'>
-                <a href='https://www.instagram.com/zbarovsky/' target='_blank'><img className='socialMedia' src='/instagram.svg'/></a>
-                <a href='https://twitter.com/zbarovsky' target='_blank'><img className='socialMedia' src='/twitter.svg' /></a>
-                <a href='https://github.com/zbarovsky' target='_blank'><img className='socialMedia' src='/010-github.png' /></a>
-                <a href='https://www.linkedin.com/in/zbarovsky/' target='_blank'><img className='socialMedia' src='013-linkedin.png' /></a>
-                <a href='mailto:zachary.barovsky@gmail.com' target='_blank'><img className='socialMedia' src='email.svg' /></a>
-                <a href='/resume.pdf' target='_blank'><img className='socialMedia' src='resume.svg' /></a>
+                <a href='https://www.instagram.com/zbarovsky/' target='_blank'><img className='socialMedia' src='/instagram.svg' alt='link for instagram profile'/></a>
+                <a href='https://twitter.com/zbarovsky' target='_blank'><img className='socialMedia' src='/twitter.svg' alt='link for twitter profile' /></a>
+                <a href='https://github.com/zbarovsky' target='_blank'><img className='socialMedia' src='/010-github.png' alt='link for github profile' /></a>
+                <a href='https://www.linkedin.com/in/zbarovsky/' target='_blank'><img className='socialMedia' src='013-linkedin.png' alt='link for linkedin profile' /></a>
+                <a href='mailto:zachary.barovsky@gmail.com' target='_blank'><img className='socialMedia' src='email.svg' alt='link to open up email for zachary.barovsky@gmail.com' /></a>
+                <a href='/resume.pdf' target='_blank'><img className='socialMedia' src='resume.svg' alt='link for pdf of resume' /></a>
             </div>
             <div className='flaticon mt-3 ml-3'>
                 Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
