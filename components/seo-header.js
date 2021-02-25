aaeaee217296d509eb91bb5f5a9c96255cf04161
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-02-25 10:17:18 -0800

    move tag for correct seo return

--- components/seo-header.js
+++ components/seo-header.js
@@ -5,8 +5,8 @@ export default function Meta () {
         <Head>
             <meta name='viewport' content='width=device-width, initial-scale=1'/>
             <meta charSet='utf-8' />
-            <meta name='description' content='Web developer based in the Seattle area who has a passion for building modern feeling full stack web applications that will fit any users need.' />
             <title>Zack Barovsky</title>
+            <meta name='description' content='Web developer based in the Seattle area who has a passion for building modern feeling full stack web applications that will fit any users need.' />
             <link rel='icon' href='/PortfolioIcon.png'/>
         </Head>
     )
