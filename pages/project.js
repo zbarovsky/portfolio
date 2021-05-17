6c900dbd1f317bb05b637d3df8d5f23e0cc77378
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-05-17 14:02:01 -0700

    started process of fixing projects section by getting rid of the ugly carousel

--- pages/project.js
+++ pages/project.js
@@ -3,54 +3,17 @@ import {Container, Row, Col, Image, Carousel} from 'react-bootstrap';
 
 export default function Project() {
 
+    const projects = [
+        {title: 'Dungeons and Dragons Encounter Tracker', link: 'https://github.com/zbarovsky/encounter-tracker', desc: 'A Dungeons and Dragons combat encounter tracker web application created to help game masters track combat scenarios for their players. It was built with EJS, NodeJs, along with a SQL Postgres database with API calls to the dungeons and dragons API.', smallImg: '/small-imgs/ETSmall.png', largeImg: '/large-imgs/ETLarge.png', alt: 'screen shot of encouter tracker website'},
+        {title: 'Stay Inside', link: 'https://github.com/zbarovsky/stay-inside-frontend', desc: 'Built in a small team of four developers, stay inside is a social media web app designed to schedule online group events with others. The front end is built using JavaScript and React. The backend is built in a NoSQL Mongo Database with a API built from scratch for the project.', smallImg: '/small-imgs/StayInsideSmall.png', largeImg: '/large-imgs/StayInsideLarge.png', alt: 'screen shot for stay inside web page'},
+        {title: 'Cryptocurrency Converter', link: 'https://github.com/zbarovsky/cryptocurrency_converter', desc: 'Cryptocurrency conversion between two types of coins. Built with react frontend and coinpaprika api', smallImg: '/small-imgs/CryptoSmall.png', largeImg: '/large-imgs/CryptoLarge.png', alt: 'scrren shot for cryptocurrency converter web page with a link to github on click'},
+        {title: 'Bloom', link: 'https://github.com/zbarovsky/GKI-tracker', desc: 'Bloom is a web app that utilized D3 to chart a users GKI levels over time based on user input.', smallImg: '/small-imgs/BloomSmall.png', largeImg: '/large-imgs/BloomLarge.png', alt: 'screen shot for bloom users profile page'},
+        {title: 'Coping Skills Generator', link: 'https://github.com/zbarovsky/coping-skills-react', desc: 'Web app designed to quick suggest coping skills to individuals.', smallImg: '/small-imgs/CSGSmall.png', largeImg: '/large-imgs/CSGLarge.png', alt: 'landing page for coping skills generator web application'},
+    ]
+
     return (
         <div id='project' className='projects'>
-            <Container fluid className="projectsContainer">
-                <h3>Recent Projects</h3><p className='learn-more'>(Click to learn more)</p>
-                <Carousel>
-                    <Carousel.Item>
-                        <a href='https://github.com/zbarovsky/encounter-tracker' rel='noopener' target='_blank'><img className='proj-img mx-auto d-block' src='/encoutertracker.png' alt='screen shot of encouter tracker website' /> </a>
-                        <Carousel.Caption>
-                            <h2 className='proj-desc'>Encounter Tracker</h2>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-
-                    <Carousel.Item>
-                        <a href='https://github.com/zbarovsky/stay-inside-frontend' rel='noopener' target='_blank'><img className='proj-img mx-auto d-block' src='/stayinside.png' alt='screen shot for stay inside web page' /></a>
-                        <Carousel.Caption>
-                            <h2 className='proj-desc'>Stay Inside</h2>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-
-                    <Carousel.Item>
-                        <a href='https://github.com/zbarovsky/middleground' rel='noopener' target='_blank'><img className='proj-img mx-auto d-block' src='/breakingbread.png' alt='screen shot for breaking bread web page' /></a>
-                        <Carousel.Caption>
-                            <h2 className='proj-desc'>Breaking Bread</h2>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-
-                    <Carousel.Item>
-                        <a href='https://github.com/zbarovsky/help-wanted-frontend' rel='noopener' target='_blank'><img className='proj-img mx-auto d-block' src='/helpwanted.png' alt='screen shot for help wanted web page' /></a>
-                        <Carousel.Caption>
-                            <h2 className='proj-desc'>Help Wanted</h2>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-
-                    <Carousel.Item>
-                        <a href='https://github.com/zbarovsky/cryptocurrency_converter' rel='noopener' target='_blank'><img className='proj-img mx-auto d-block' src='/cryptoconverter.png' alt='scrren shot for cryptocurrency converter web page with a link to github on click' /></a>
-                        <Carousel.Caption>
-                            <h2 className='proj-desc'>Cryptocurrency Converter</h2>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    
-                    <Carousel.Item>
-                        <a href='https://github.com/zbarovsky/GKI-tracker' rel='noopener' target='_blank'><img className='proj-img mx-auto d-block' src='/bloom.png' alt='screen shot for bloom users profile page' /></a>
-                        <Carousel.Caption>
-                            <h2 className='proj-desc'>Bloom</h2>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </Container>
+            
         </div>
     )
 }
